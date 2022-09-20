6304bfcf321cac1e7c7e03092beeb2bfda393cc5
jakebush1998 <Student@LAPTOP-CB78AN7D>
2022-09-20 09:35:49 -0500

    Got contact form functioning, minor error to fix with the form giving an error even though it's sending the email

--- PersonalSite.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite.UI.MVC/Controllers/HomeController.cs
@@ -30,6 +30,77 @@ namespace PersonalSite.UI.MVC.Controllers
 
         }
 
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            //Variable for the body of the email 
+            string message = $"Hello! You have received a new email from your website's contact form! <br />" +
+                $"Sender: {cvm.Name}<br />Email: {cvm.Email}<br />Subject: {cvm.Subject}<br />Message: {cvm.Message}<br /><br />" +
+                $"<strong> DO NOT REPLY TO THIS EMAIL. Send replies to {cvm.Email}</strong>";
+
+            //Created an instance of MimeMessage that will store all of the email object's info
+            var msg = new MimeMessage();
+
+            //Assign credentials to send the email 
+            msg.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+
+            msg.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            msg.Subject = cvm.Subject;
+
+            msg.Body = new TextPart("HTML") { Text = message };
+
+            msg.Priority = MessagePriority.Urgent;
+
+
+            //Attempt to send the email 
+            using (var client = new SmtpClient())
+            {
+                //Connect to the mail server 
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"));
+
+                //Authenticate our email user
+                client.Authenticate(
+                    //Username 
+                    _config.GetValue<string>("Credentials:Email:User"),
+
+                    //Password
+                    _config.GetValue<string>("Credentials:Email:Password")
+                    );
+
+                try
+                {
+                    client.Send(msg);
+                }
+                catch (Exception ex)
+                {
+                    //If for any reason the client.Send(msg) fails, this code will execute and allow us to exit gracefully
+                    //without causing a Runtime Exception 
+                    ViewBag.ErrorMessage = $"There was an error processing your request. Please try again later.<br />" +
+                        $"Error Message: {ex.StackTrace}";
+
+                    return View(cvm);
+                }
+
+            }
+
+            //If all goes well, return a View that displays a confirmation of the message being sent 
+            return View("EmailConfirmation", cvm);
+
+            //Email - Step 10 
+            //Add the EmailConfirmation view
+            // - Right click here in the Action -> Add View 
+            // - Select Razor View - Empty 
+            // - Name: EmailConfirmation 
+            // - Add the @model decleration to the View 
+            // - Code the view's HTML 
+
+        }
 
 
         public IActionResult Index()
